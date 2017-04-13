eac9099668164eceddc2ab017ec8f09ba1184247
ivansandrini <ivansandrini@gmail.com>
2017-04-12 22:21:50 -0300

    capitulo 2

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+    
+      <ul>
+        <li v-for="foto of fotos">
+          <img :src="foto.url" :alt="foto.titulo">
+        </li>
+      </ul>
+    
+  </div>
+</template>
+
+<script>
+export default {
+
+    data() {
+      return {
+        fotos: []
+      }
+    },
+
+    created() {
+      let promise = this.$http.get('http://localhost:3000/v1/fotos')
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos, err => console.err(err));
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
